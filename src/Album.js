9bce594e9b3d1013abba79fdb376bca3b5e3bd30
kumiko.anzai <anzai.k@opst.co.jp>
2020-07-24 17:16:53 +0900

    feat: add image uploader

--- src/Album.js
+++ src/Album.js
@@ -13,13 +13,14 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
+import ImageUpload from "./ImageUpload";
 
 function Copyright() {
     return (
         <Typography variant="body2" color="textSecondary" align="center">
             {'Copyright © '}
-            <Link color="inherit" href="https://material-ui.com/">
-                AliEaters Code支部 #3
+            <Link color="inherit" href="https://www.alieaters.com/">
+                AliEaters Code支部
             </Link>{' '}
             {new Date().getFullYear()}
             {'.'}
@@ -61,6 +62,8 @@ const useStyles = makeStyles((theme) => ({
 
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+const galleryImageList = [];
+
 export default function Album() {
     const classes = useStyles();
 
@@ -82,6 +85,7 @@ export default function Album() {
                         <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
                             Album layout
                         </Typography>
+                        <ImageUpload cardName="Input Image" imageGallery={galleryImageList} />
                         <Typography variant="h5" align="center" color="textSecondary" paragraph>
                             Something short and leading about the collection below—its contents, the creator, etc.
                             Make it short and sweet, but not too short so folks don&apos;t simply skip over it
@@ -139,10 +143,10 @@ export default function Album() {
             {/* Footer */}
             <footer className={classes.footer}>
                 <Typography variant="h6" align="center" gutterBottom>
-                    Footer
+                    AliEaters - Alibaba Cloud Developers Community
                 </Typography>
                 <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
-                    Something here to give the footer a purpose!
+                    Presented by Hiro, nakajima airi, terao, watanabe, yoshimura, yuichi.big zakky and anzoo
                 </Typography>
                 <Copyright />
             </footer>
